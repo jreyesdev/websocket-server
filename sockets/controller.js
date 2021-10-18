38da07f7d4abb5c4a5c8c4537d750784b818de2e
jreyesdev <djrm1204@gmail.com>
2021-10-18 19:40:32 -0400

    Inicio aplicacion de cola

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,8 +1,4 @@
 const socketController = socket => {
-    console.log('Cliente conectado ',socket.id)
-    socket.on('disconnect',()=> {
-        console.log('Cliente desconectado ',socket.id)
-    })
     socket.on('enviar-mensaje',(pay,callback) => {
         socket.broadcast.emit('enviar-mensaje',pay)
         callback({ id: 123, fecha: new Date().getTime() })
