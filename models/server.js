770f8394c2611fbaa57bd43b177706bfdf48620f
jreyesdev <djrm1204@gmail.com>
2021-10-18 18:18:40 -0400

    Socket.io

--- models/server.js
+++ models/server.js
@@ -4,8 +4,10 @@ const cors = require('cors')
 
 class Server{
     constructor(){
-        this.app = express()
         this.port = process.env.APP_PORT
+        this.app = express()
+        this.server = require('http').createServer(this.app)
+        this.io = require('socket.io')(this.server)
         this.middlewares()
         this.routes()
     }
@@ -19,7 +21,7 @@ class Server{
     routes(){}
 
     listen(){
-        this.app.listen(this.port,() => console.log(`Server on port ${this.port}`))
+        this.server.listen(this.port,() => console.log(`Server on port ${this.port}`))
     }
 }
 
