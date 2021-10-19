22769527ced75b17260d38a19280b65022cb3a86
jreyesdev <djrm1204@gmail.com>
2021-10-18 23:14:56 -0400

    Atender tickets

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,45 @@
+const lblEscritorio = document.querySelector('h1');
+const btn = document.querySelector('button');
+const lblSmall = document.querySelector('small');
+const lblAlert = document.querySelector('#alert');
+const lblPendientes = document.querySelector('#lblPendientes');
+const search = window.location.search.replace('?','').split('&')
+
+search.forEach((e,i)=>{
+    search[i] = e.split('=')
+})
+const [escritorio] = search.filter(e => e[0] === 'escritorio')
+
+if(!escritorio || isNaN(escritorio[1]) ){
+    window.location = '/'
+    throw new Error('El escritorio es obligatorio y debe ser numerico')
+}
+
+lblEscritorio.innerText = `${escritorio.join(' ').toUpperCase()}`
+const socket = io();
 
 
-console.log('Escritorio HTML');
+socket.on('connect',()=>{
+    btn.disabled = false;
+});
+
+socket.on('disconnect',()=>{
+    btn.disabled = true;
+});
+
+socket.on('pendi-ticket',ticket => {
+    lblPendientes.innerText = ticket
+})
+
+btn.addEventListener('click', e => {
+    socket.emit('atender-ticket',{ escritorio }, pay => {
+        const { status, ticket, msg } = pay
+        if(!status){
+            lblAlert.innerText = msg
+            lblAlert.style.display = 'block'
+            lblSmall.innerText = 'Nadie'
+            return
+        }
+        lblSmall.innerText = 'Ticket ' + ticket.numero
+    });
+})
--- sockets/controller.js
+++ sockets/controller.js
@@ -3,11 +3,36 @@ const ticket = new ticketControl()
 
 const socketController = socket => {
     socket.emit('ultimo-ticket',ticket.ultimo)
+
+    socket.emit('pendi-ticket',ticket.tickets.length)
     
     socket.on('sig-ticket',(pay,callback) => {
         const siguiente = ticket.siguiente()
         callback(siguiente)
         // Todo: notificar que hay nuevo ticket por atender
+    })
+    
+    socket.on('atender-ticket',({ escritorio },callback) => {
+        const resp = {}
+        if(!escritorio){
+            resp.status = false
+            resp.msg = 'El escritorio es obligatorio y debe ser numerico'
+        }
+        if(!resp.length){
+            const atender = ticket.atenderTicket(escritorio.join(' '))
+
+            // Notificar actualizar ultimos 4
+
+            if(!atender){
+                resp.status = false
+                resp.msg = 'Ya no hay tickets que atender'
+            }else{
+                resp.status = true
+                resp.ticket = atender
+            }
+        }
+        socket.emit('pendi-ticket',ticket.tickets.length)
+        callback(resp)
     })
 }
 
